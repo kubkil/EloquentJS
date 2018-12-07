17e17b2bfb7d0f59a1206be34e9e5c01e4255abf
Jakub Kilar <kuba.kilar@gmail.com>
2018-12-07 18:17:06 +0100

    Add solution of second exercise

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 // #1
-let branch = '';
-for (line = 0; line < 8; line++) {
-  branch += '#';
-  console.log(branch);
-}
+// let branch = '';
+// for (line = 0; line < 8; line++) {
+//   branch += '#';
+//   console.log(branch);
+// }
 
 // #2
+for (let i = 1; i < 101; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log('FizzBuzz');
+  } else if (i % 3 === 0) {
+    console.log('Fizz');
+  } else if (i % 5 === 0) {
+    console.log('Buzz');
+  } else {
+    console.log(i);
+  }
+}
