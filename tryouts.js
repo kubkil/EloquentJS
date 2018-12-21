d1e271a0c702e2fabb22313908d3bf6a48e3502a
Jakub Kilar <kuba.kilar@gmail.com>
2018-12-21 17:41:26 +0100

    Try power function

---
+++ tryouts.js
@@ -0,0 +1,11 @@
+//#1. Create a function that multiplies a number by itself given number of times
+
+// const power = (base, exponent) => {
+//   let result = 1;
+//   for (let i = 0; i < exponent; i++) {
+//     result *= base;
+//   }
+//   return result;
+// };
+
+// power(2, 4);
