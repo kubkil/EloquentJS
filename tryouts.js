2c6f411b1cebdd089389795785b49ff2abdbab87
Jakub Kilar <kuba.kilar@gmail.com>
2018-12-22 16:59:27 +0100

    Tryouts

--- tryouts.js
+++ tryouts.js
@@ -6,22 +6,47 @@
 //   }
 //   return result;
 // };
-
 // power(2, 4);
 
 // #2 Hummus
-const hummus = factor => {
-  const ingredient = (amount, unit, name) => {
-    let ingredientAmount = amount * factor;
-    if (ingredientAmount > 1) {
-      unit += 's';
-    }
-    console.log(`${ingredientAmount} ${unit} ${name}`);
-  };
-  ingredient(1, 'can', 'chickpeas');
-  ingredient(0.25, 'cup', 'tahini');
-  ingredient(0.25, 'cup', 'lemon juice');
-  ingredient(1, 'clove', 'garlic');
-  ingredient(2, 'tablespoon', 'olive oil');
-  ingredient(0.5, 'teaspoon', 'cumin');
-};
+// const hummus = factor => {
+//   const ingredient = (amount, unit, name) => {
+//     let ingredientAmount = amount * factor;
+//     if (ingredientAmount > 1) {
+//       unit += 's';
+//     }
+//     console.log(`${ingredientAmount} ${unit} ${name}`);
+//   };
+//   ingredient(1, 'can', 'chickpeas');
+//   ingredient(0.25, 'cup', 'tahini');
+//   ingredient(0.25, 'cup', 'lemon juice');
+//   ingredient(1, 'clove', 'garlic');
+//   ingredient(2, 'tablespoon', 'olive oil');
+//   ingredient(0.5, 'teaspoon', 'cumin');
+// };
+
+// #3 Imitate minus
+// function minus(a, b) {
+//   if (b === undefined) return -a;
+//   else return a - b;
+// }
+
+// #4 Multiplier closure
+// function multiplier(factor) {
+//   return number => number * factor;
+//   // return function(number) {
+//   //   return number * factor;
+//   // };
+// }
+
+// let twice = multiplier(5);
+// // let twice = number => number * 5;
+// console.log(twice(2));
+
+// #4 recursion
+// function power(base, exponent) {
+//   if (exponent === 0) return 1;
+//   else return base * power(base, exponent - 1);
+// }
+
+// console.log(power(2, 5));
