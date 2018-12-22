33dd57f5ead94f592fb0193d81ae51040469f30d
Jakub Kilar <kuba.kilar@gmail.com>
2018-12-22 13:26:21 +0100

    Add second tryout

--- tryouts.js
+++ tryouts.js
@@ -1,5 +1,4 @@
-//#1. Create a function that multiplies a number by itself given number of times
-
+// #1. Create a function that multiplies a number by itself given number of times
 // const power = (base, exponent) => {
 //   let result = 1;
 //   for (let i = 0; i < exponent; i++) {
@@ -9,3 +8,20 @@
 // };
 
 // power(2, 4);
+
+// #2 Hummus
+const hummus = factor => {
+  const ingredient = (amount, unit, name) => {
+    let ingredientAmount = amount * factor;
+    if (ingredientAmount > 1) {
+      unit += 's';
+    }
+    console.log(`${ingredientAmount} ${unit} ${name}`);
+  };
+  ingredient(1, 'can', 'chickpeas');
+  ingredient(0.25, 'cup', 'tahini');
+  ingredient(0.25, 'cup', 'lemon juice');
+  ingredient(1, 'clove', 'garlic');
+  ingredient(2, 'tablespoon', 'olive oil');
+  ingredient(0.5, 'teaspoon', 'cumin');
+};
