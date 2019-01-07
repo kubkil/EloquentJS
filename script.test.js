beb2011d5ebebe759ffc6990bc3146a74054fd3b
Jakub Kilar <kuba.kilar@gmail.com>
2019-01-07 19:18:35 +0100

    Add mocha test for 5th exercise

---
+++ script.test.js
@@ -0,0 +1,32 @@
+const assert = require('assert');
+const isEven = require('./script');
+
+describe('isEven', () => {
+  it('should return false to uneven number', () => {
+    const actual = isEven(21);
+    const expected = false;
+
+    assert.equal(actual, expected);
+  });
+
+  it('should return true to even number', () => {
+    const actual = isEven(20);
+    const expected = true;
+
+    assert.equal(actual, expected);
+  });
+
+  it('should return false to negative uneven number', () => {
+    const actual = isEven(-21);
+    const expected = false;
+
+    assert.equal(actual, expected);
+  });
+
+  it('should return true to negative even number', () => {
+    const actual = isEven(-20);
+    const expected = true;
+
+    assert.equal(actual, expected);
+  });
+});
