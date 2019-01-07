ab33189fdd65c719e94efc63a21e7aa243cad4a9
Jakub Kilar <kuba.kilar@gmail.com>
2019-01-07 19:19:02 +0100

    Comment out

--- tryouts.js
+++ tryouts.js
@@ -52,20 +52,22 @@
 // console.log(power(2, 5));
 
 // #5 Write a function that, given a number, tries to find a sequence of such additions and multiplications that produces that number
-function findSolution(target) {
-  function find(current, history) {
-    if (current === target) {
-      return history;
-    } else if (current > target) {
-      return null;
-    } else {
-      return (
-        find(current + 5, `(${history} + 5)`) ||
-        find(current * 3, `${history} * 3)`)
-      );
-    }
-  }
-  return find(1, '1');
-}
+// function findSolution(target) {
+//   function find(current, history) {
+//     if (current === target) {
+//       return history;
+//     } else if (current > target) {
+//       return null;
+//     } else {
+//       return (
+//         find(current + 5, `(${history} + 5)`) ||
+//         find(current * 3, `${history} * 3)`)
+//       );
+//     }
+//   }
+//   return find(1, '1');
+// }
+
+// findSolution(24);
 
 findSolution(24);
