5a4b293a87c2f0996da2c443fa244591990056d5
Jakub Kilar <kuba.kilar@gmail.com>
2019-01-20 16:54:39 +0100

    Require countChar

--- exercises.test.js
+++ exercises.test.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const isEven = require('./exercises');
+const countChar = require('./exercises');
 
 describe('isEven', () => {
   it('should return false to uneven number', () => {
