f92c8ee4f5353c6ab0a5adc94ed657ec485107c9
Jakub Kilar <kuba.kilar@gmail.com>
2019-01-20 16:54:57 +0100

    Comment out

--- exercises.test.js
+++ exercises.test.js
@@ -2,27 +2,35 @@ const assert = require('assert');
 const isEven = require('./exercises');
 const countChar = require('./exercises');
 
-describe('isEven', () => {
-  it('should return false to uneven number', () => {
-    const actual = isEven(21);
-    const expected = false;
+// describe('isEven', () => {
+//   it('should return false to uneven number', () => {
+//     const actual = isEven(21);
+//     const expected = false;
 
-    assert.equal(actual, expected);
-  });
+//     assert.equal(actual, expected);
+//   });
 
-  it('should return true to even number', () => {
-    const actual = isEven(20);
-    const expected = true;
+//   it('should return true to even number', () => {
+//     const actual = isEven(20);
+//     const expected = true;
 
-    assert.equal(actual, expected);
-  });
+//     assert.equal(actual, expected);
+//   });
 
-  it('should return false to negative uneven number', () => {
-    const actual = isEven(-21);
-    const expected = false;
+//   it('should return false to negative uneven number', () => {
+//     const actual = isEven(-21);
+//     const expected = false;
 
-    assert.equal(actual, expected);
-  });
+//     assert.equal(actual, expected);
+//   });
+
+//   it('should return true to negative even number', () => {
+//     const actual = isEven(-20);
+//     const expected = true;
+
+//     assert.equal(actual, expected);
+//   });
+// });
 
 describe('countChar', () => {
   it('should return 2 to "asdgTT"', () => {
