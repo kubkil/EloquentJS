24b4eafed5eff178d03e4629998994be66460e49
Jakub Kilar <kuba.kilar@gmail.com>
2019-01-28 20:08:11 +0100

    Add 7th tryout

--- tryouts.mjs
+++ tryouts.mjs
@@ -119,8 +119,6 @@
 // #7 Weresquirrel
 import { journal } from './journal.mjs';
 
-console.log(journal);
-
 function addEntry(events, squirrel) {
   journal.push({ events, squirrel });
 }
@@ -135,4 +133,54 @@ function phi(table) {
         (table[0] + table[2])
     )
   );
+}
+
+function tableFor(event, journal) {
+  let table = [0, 0, 0, 0];
+  for (let i = 0; i < journal.length; i++) {
+    let entry = journal[i],
+      index = 0;
+    if (entry.events.includes(event)) index += 1;
+    if (entry.squirrel) index += 2;
+    table[index] += 1;
+  }
+  return table;
+}
+
+function journalEvents(journal) {
+  let events = [];
+  for (let entry of journal) {
+    for (let event of entry.events) {
+      if (!events.includes(event)) {
+        events.push(event);
+      }
+    }
+  }
+  return events;
+}
+
+function max(...numbers) {
+  let result = -Infinity;
+  for (let number of numbers) {
+    if (number > result) result = number;
+  }
+  return result;
+}
+
+var list = {
+  value: 1,
+  rest: {
+    value: 2,
+    rest: {
+      value: 3,
+      rest: null
+    }
+  }
+};
+
+for (let event of journalEvents(journal)) {
+  let correlation = phi(tableFor(event, journal));
+  if (correlation > 0.1 || correlation < -0.1) {
+    console.log(event + ':', correlation);
+  }
 }
