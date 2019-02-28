eca86a0c1a5b27752134194319978f8b9d69b84c
Jakub Kilar <kuba.kilar@gmail.com>
2019-02-28 22:08:57 +0100

    Add test to reverseArray and reverseArrayInPlace

--- exercises.test.js
+++ exercises.test.js
@@ -72,3 +72,21 @@ describe('sum', () => {
     assert.equal(actual, expected);
   });
 });
+
+describe('reverseArray', () => {
+  it('should return new array [5, 4, 3, 2, 1] to "[1, 2, 3, 4, 5]"', () => {
+    const actual = functions.reverseArray([1, 2, 3, 4, 5]);
+    const expected = [5, 4, 3, 2, 1];
+
+    expect(actual).to.have.members(expected);
+  });
+});
+
+describe('reverseArrayInPlace', () => {
+  it('should return the same but reversed array [7, 6, 5, 4, 3, 2, 1] to "[1, 2, 3, 4, 5, 6, 7]"', () => {
+    const actual = functions.reverseArrayInPlace([1, 2, 3, 4, 5, 6, 7]);
+    const expected = [7, 6, 5, 4, 3, 2, 1];
+
+    expect(actual).to.have.members(expected);
+  });
+});
