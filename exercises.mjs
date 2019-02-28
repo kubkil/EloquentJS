a426308b0abc57cb3061ce21d7777500774e34a4
Jakub Kilar <kuba.kilar@gmail.com>
2019-02-28 20:02:24 +0100

    Add first solution for 8th exercise

--- exercises.mjs
+++ exercises.mjs
@@ -117,6 +117,15 @@
 //   return total;
 // };
 
+// #8 Reversing an array
+
+const reverseArray = arr => {
+  let reversed = [];
+  for (let i = arr.length - 1; i >= 0; i--) {
+    reversed.push(arr[i]);
+  }
+  return reversed;
+};
 // module.exports = {
 //   // isEven,
 //   // countChar,
