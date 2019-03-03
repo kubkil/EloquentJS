7d61bf425a3c4d78fad98c2984ac22517c51f8b6
Jakub Kilar <kuba.kilar@gmail.com>
2019-03-03 22:46:49 +0100

    Add first part of 9nt exercise

--- exercises.mjs
+++ exercises.mjs
@@ -132,11 +132,18 @@ const reverseArrayInPlace = arr => {
   return arr;
 };
 
-module.exports = {
-  isEven,
-  countChar,
-  range,
-  sum,
-  reverseArray,
-  reverseArrayInPlace
+// #9 A list
+
+const arrayToList = arr => {
+  let obj;
+  for (let i = arr.length - 1; i >= 0; i--) {
+    obj = {
+      value: arr[i],
+      rest: obj
+    };
+  }
+  if (obj.rest == {}) {
+    obj.rest = null;
+  }
+  console.log(obj);
 };
