0c665518c8ee375496904a96d60346797c13389c
Jakub Kilar <kuba.kilar@gmail.com>
2019-03-10 19:41:20 +0100

    Add test for prepend

--- exercises.test.js
+++ exercises.test.js
@@ -117,3 +117,12 @@ describe('listToArray', () => {
     expect(actual).to.eql(expected);
   });
 });
+
+describe('prepend', () => {
+  it('should return {value: 10, rest: {value: 20, rest: null}} to (10, prepend(20, null));', () => {
+    const actual = functions.prepend(10, functions.prepend(20, null));
+    const expected = { value: 10, rest: { value: 20, rest: null } };
+
+    expect(actual).to.eql(expected);
+  });
+});
